b8f821b6d4cab6680e8138be37895cfbe8de1717
Raja Shravan <rajashravan@hotmail.com>
2020-03-16 15:53:48 -0400

    add more and tests

--- hello.go
+++ hello.go
@@ -1,9 +1,17 @@
 package main
+// use 'package main' for executable commands.
+// is hello.go considered a package? I think it is but idk
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/rajashravan/hello/morestrings"
+  "github.com/google/go-cmp/cmp" // remote repo example
+)
 
 func main() {
 	fmt.Println("Hello, world.")
-
-	fmt.Println(1+'1')
+  fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+  fmt.Println('1'+2)
+  fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
